2419a025c78a8016c88e90237bcf0a44a507d81e
Yurii Soroka <yurisoroka165@gmail.com>
2023-02-27 13:42:06 +0200

    initial commit

---
+++ src/components/FriendList/FriendList.jsx
@@ -0,0 +1,22 @@
+import PropTypes from 'prop-types';
+import css from './FriendList.module.css';
+import { FriendListItem } from 'components/FriendListItem/FriendListItem';
+
+export const FriendList = ({ friends }) => {
+  return (
+    <ul className={css.friend_list}>
+      {friends.map(({ avatar, name, isOnline, id }) => (
+        <FriendListItem
+          key={id}
+          avatar={avatar}
+          name={name}
+          isOnline={isOnline}
+        />
+      ))}
+    </ul>
+  );
+};
+
+FriendList.propTypes = {
+  friends: PropTypes.array.isRequired,
+};
